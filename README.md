9909a167b14a6b21b4016830b1f0bd4d522ae4b3
joannadelaune <102549713+joannadelaune@users.noreply.github.com>
2023-09-06 18:34:09 -0500

    Project complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# sqlalchemy-challenge
- 
+# Data Analysis with SQLAlchemy
+
+The first part of this project uses SQLAlchemy, Pandas, and Matplotlib to explore and analyze a dataset of temperature and precipitation data from weather stations in Hawaii.
+
+The second part is a Flask API with routes that return a list of stations, a year's worth of temperature or precipitation observations, or permit the user to input a pair of dates and get the minimum, average, and maximum temperature between the two dates (or between a start date and the end date present in the dataset).
+
+## Acknowledgements
+
+Thanks to Kourt Bailey for advice in restructuring the code to make it cooler and awesomer.
--- app.py
+++ app.py
@@ -64,12 +64,13 @@ app = Flask(__name__)
 def tobs():
     """Query the dates and temperature observations of the most-active station for the previous year of data. 
     Return a JSON list of temperature observations for the previous year."""
-    with Session() as session:
-        
-        # Find date one year from latest date in dataset
-        latest_date = most_recent_date()
-        start_date = one_year_prev(latest_date)
 
+    # Find date one year from latest date in dataset
+    latest_date = most_recent_date()
+    start_date = one_year_prev(latest_date)
+
+    with Session() as session:
+    
         # find most active station
         active_stations_query = session.query(Measurement.station, func.count(Measurement.station))\
             .group_by(Measurement.station)\
@@ -96,31 +97,27 @@ def tobs():
 def tstats(start_date, end_date=str(most_recent_date())):
     """Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature
     from a specified start date to the most recent date in the database."""
-    print(end_date)
-    print(str(type(end_date)))
+
+    print(f'start date {start_date} and end date {end_date}')
     with Session() as session:
         tstats_result = session.query(
-            func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs))\
+            func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
             .filter(Measurement.date >= start_date)\
             .filter(Measurement.date <= end_date)\
             .first()
 
-    return jsonify(tuple(tstats_result))    
-
-    # For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
-
-    # For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date
-    # to the end date, inclusive.
+    return jsonify(tuple(tstats_result))
 
 @app.route("/api/v1.0/precipitation")
 def precip():
     """Return the most recent year's worth of daily precipitation data from the dataset in JSON format"""
+
+    # Find date one year from latest date in dataset
+    latest_date = most_recent_date()
+    start_date = one_year_prev(latest_date)
+
     with Session() as session:
-        
-        # Find date one year from latest date in dataset
-        latest_date = most_recent_date()
-        start_date = one_year_prev(latest_date)
-        
+           
         # Perform a query to retrieve the data and precipitation scores
         last_year_precip = session.query(Measurement.date, func.avg(Measurement.prcp))\
             .filter(Measurement.date >= start_date)\
@@ -152,13 +149,22 @@ def stations():
 @app.route("/")
 def welcome():
     return (
-        f'''<h1>Welcome to the Generic API!</h1>
+        f'''<h1>Welcome to the Hawaii Weather API</h1>
         <p>Available Routes:<br/>
-        <ul>
-        <li><b>Route 1:</b>
-        <ul style="list-style-type:none"><li>/url1</ul><br />
-        <li><b>Route 2:</b>
-        <ul style="list-style-type:none"><li>/url2</ul>
+        <ul style="list-style-type:none">
+        <li><b>/api/v1.0/stations</b>
+        <ul style="list-style-type:none"><li>Returns a JSON list of all weather stations in the dataset.</ul><br />
+        <li><b>/api/v1.0/precipitation</b>
+        <ul style="list-style-type:none"><li>Returns precipitation data for the most recent year present in the dataset in JSON format.</ul><br />
+        <li><b>/api/v1.0/tobs</b>
+        <ul style="list-style-type:none"><li>Returns a JSON list of temperature observations at the most active station for the most
+        recent year present in the dataset.</ul><br />
+        <li><b>/api/v1.0/tstats/&lt;start_date&gt;</b> or <b>/api/v1.0/tstats/&lt;start_date&gt;/&lt;end_date&gt;</b>
+        <ul style="list-style-type:none"> <li>Use <b>yyyy-mm-dd</b> format for dates.<br />
+        <li>Returns a JSON list with the minimum, the average, and the maximum of all temperature
+        observations between the start and end date. If no end date is given, the most recent date for which an
+        observation exists in the dataset is used as the end date.<br /></ul>
+    
         </ul></p>'''
     )   
 
